681d9d73f40bd065045ceccd6de2221c1c73d880
pixegami <pixegami@gmail.com>
2021-08-06 18:46:58 +1000

    Add random drink function

---
+++ netlify/functions/hello-world/hello-world.js
@@ -0,0 +1,15 @@
+const handler = async (event) => {
+  try {
+    const drinks = ["coffee", "tea", "beer", "wine", "water", "juice"];
+    const drink = drinks[Math.floor(Math.random() * items.length)];
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `You should drink some ${drink}` }),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
+
+module.exports = { handler };
